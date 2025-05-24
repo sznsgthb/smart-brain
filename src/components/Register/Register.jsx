106dbe60e64a2a2d8a7bef827e93fd4820b3df5a
sznsgthb <suzannehoftijzer@gmail.com>
2025-05-24 15:01:48 +0200

    updating endpoints

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -25,7 +25,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://smart-brain-api-foy8.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -75,7 +75,7 @@ class App extends Component {
     onPictureSubmit = () => {
         this.setState({imageUrl: this.state.input});
         // app.models.predict('face-detection', this.state.input)
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://smart-brain-api-foy8.onrender.com/imageurl', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ input: this.state.input })
@@ -83,7 +83,7 @@ class App extends Component {
           .then(response => response.json())
           .then(response => {
             if (response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://smart-brain-api-foy8.onrender.com/image', {
                     method: 'put',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({ id: this.state.user.id })
